10646fff0c31c6065e6ae82b53e815b8ee24479c
Francososa <franky.sosa@gmail.com>
2018-09-17 19:17:54 -0600

    Define properties to test in isPalindrome function

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,19 @@
 import Lib
+import Data.Char (isPunctuation)
+
 
 assert :: Bool -> String -> String -> IO ()
 assert test passStatement failStatement = if test
                                            then putStrLn passStatement
                                            else putStrLn failStatement
+
+prop_punctuationInvariant text = preprocess text ==
+                                   preprocess noPuncText
+  where noPuncText = filter (not . isPunctuation) text
+                                                
+prop_reverseInvariant text = isPalindrome text ==
+                             (isPalindrome (reverse text))
+                             
 
 main :: IO ()
 main = do

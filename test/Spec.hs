5753e7ad72806014424c4d18df25cc5c6b98d426
Francososa <franky.sosa@gmail.com>
2018-09-17 20:06:56 -0600

    Add tests for case sensitivity and whitespace

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,9 @@
 import Lib
 import Data.Char (isPunctuation)
+import Data.Text as T
 import Test.QuickCheck
+import Test.QuickCheck.Instances
+
 
 assert :: Bool -> String -> String -> IO ()
 assert test passStatement failStatement = if test
@@ -9,14 +12,25 @@ assert test passStatement failStatement = if test
 
 prop_punctuationInvariant text = preprocess text ==
                                    preprocess noPuncText
-  where noPuncText = filter (not . isPunctuation) text
+  where noPuncText = T.filter (not . isPunctuation) text
                                                 
 prop_reverseInvariant text = isPalindrome text ==
-                             (isPalindrome (reverse text))
-                             
+                             (isPalindrome (T.reverse text))
+
+
+prop_caseInvariant text = preprocess text ==
+                            preprocess caseLessText
+  where caseLessText = T.toLower text
+
+
+prop_whiteSpaceInvariant text = preprocess text ==
+                                  preprocess strippedText
+  where strippedText = T.strip text
 
 main :: IO ()
 main = do
   quickCheckWith stdArgs { maxSuccess = 1000 } prop_punctuationInvariant
   quickCheck prop_reverseInvariant
+  quickCheck prop_caseInvariant
+  quickCheck prop_whiteSpaceInvariant
   putStrLn "done!"
